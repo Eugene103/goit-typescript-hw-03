36af6f1ac1e77ed4aa9aa3df0a59143763f633b2
Eugene103 <chertyka74@gmail.com>
2023-12-01 18:22:19 +0200

    v1

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,43 @@
+class Key{
+    private signature: number;
+    constructor() {
+    this.signature = Math.random()
+    }
+    getSignature(): number{
+        return this.signature;
+    }
+}
+
+class Person{
+    private key: Key;
+constructor(key) {
+    this.key = key
+    }
+getKey(): Key{
+    return this.key
+}
+}
+
+abstract class House{
+    door: boolean = false;
+    tenants: Person[] = [];
+    
+    constructor(protected key: Key) { }
+    
+abstract  openDoor(key: Key): void;
+    comeIn(person: Person): void{
+        if (this.door) {
+            this.tenants.push(person);
+        }
+}
+}
+class MyHouse extends House {
+    openDoor(key: Key): void {
+        if (key.getSignature() === this.key.getSignature()) {
+            this.door = true
+        }
+    }
+}
 const key = new Key();
 
 const house = new MyHouse(key);
